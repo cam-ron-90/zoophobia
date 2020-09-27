533e5914fa9950b2fc6d8c0fe45a8af1fdfeb8da
Cameron Melville <cam.t.melville@gmail.com>
2020-09-27 20:54:38 +1000

    create-game and join-game basic setup

--- Models/Game.js
+++ Models/Game.js
@@ -6,22 +6,17 @@ const PlayerSchema = new mongoose.Schema({
     isCurrentPlayer : {type : Boolean, default : false},
     cards : [{type : String}],
     microAgressions : [{type : String}],
-    points : {type : Number},
+    points : {type : Number, default : 0},
     avatar : {
         animal : {type : String},
-        powers : {
-            physical : {type : String},
-            communication : {type : String},
-            healing : {type : String},
-            generosity : {type : String},
-            ancestral : {type : String}
-        }
+        power : {type : String}
     },
     nickName : {type : String}
 });
 
 const GameSchema = new mongoose.Schema({
     cards : [{type : String}],
+    avatarsAvailable : [{type : String}],
     isOpen : {type : Boolean, default : true},
     isOver : {type : Boolean, default : false},
     players : [PlayerSchema],
--- app.js
+++ app.js
@@ -11,3 +11,98 @@ const Game = require('./Models/Game');
 mongoose.connect('mongodb://localhost:27017/zoophobia',
                 {useNewUrlParser : true, useUnifiedTopology : true},
                 ()=>{ console.log('successfully connected to database')});
+
+
+
+
+io.on('connect',(socket)=>{
+
+    socket.on('timer', async({gameID,playerID})=>{
+        let countDown = 5;
+        let game = await Game.findById(gameID);
+        let player = game.players.id(playerID);
+        if(player.isPartyLeader){
+            let timerID = setInterval(async()=>{
+                if(countDown >= 0){
+                    io.to(gameID).emit('timer',{countDown,msg : "Starting Game"});
+                    countDown--;
+                }
+                else{
+                    game.isOpen = false;
+                    game = await game.save();
+                    io.to(gameID).emit('updateGame',game);
+                    startGameClock(gameID);
+                    clearInterval(timerID);
+                }
+            },1000);
+        }
+    });
+
+    socket.on('join-game',async ({gameID : _id,nickName, animal})=>{
+        try{
+            let game = await Game.findById(_id);
+            if(game.isOpen){
+                const gameID = game._id.toString();
+                socket.join(gameID);
+                let player = {
+                    socketID : socket.id,
+                    nickName
+                }
+                game.players.push(player);
+                game = await game.save();
+                io.to(gameID).emit('updateGame',game);
+            }
+        }catch(err){
+            console.log(err)
+        }
+    })
+
+    socket.on('create-game',async (nickName, animal)=>{
+        try{
+            let game = new Game();
+            let player = {
+                socketID : socket.id,
+                isPartyLeader : true,
+                nickName
+            }
+            game.players.push(player);
+            game = await game.save();
+            const gameID = game._id.toString();
+            socket.join(gameID);
+            io.to(gameID).emit('updateGame',game);
+        }catch(err){
+            console.log(err);
+        }
+    });
+});
+
+// const startGameClock = async (gameID)=>{
+//     let game = await Game.findById(gameID);
+//     game.startTime = new Date().getTime();
+//     game = await game.save();
+//     let time = 120;
+
+//     let timerID = setInterval(function gameIntervalFunc(){
+//         if(time >= 0){
+//             const formatTime = calculateTime(time);
+//             io.to(gameID).emit('timer',{countDown : formatTime,msg : "Time Remaining"});
+//             time--;
+//         }
+//         else{
+//             (async ()=>{
+//                 let endTime = new Date().getTime();
+//                 let game = await Game.findById(gameID);
+//                 let {startTime} = game;
+//                 game.isOver = true;
+//                 game.players.forEach((player,index)=>{
+//                     if(player.WPM === -1)
+//                         game.players[index].WPM = calculateWPM(endTime,startTime,player);
+//                 });
+//                 game = await game.save()
+//                 io.to(gameID).emit('updateGame',game);
+//                 clearInterval(timerID);
+//             })()
+//         }
+//         return gameIntervalFunc;
+//     }(),1000);
+// }
