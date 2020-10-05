22afc19e02ebf27f1ea4575f45ce7c2292b0da32
Cameron Melville <cam.t.melville@gmail.com>
2020-10-05 16:00:24 +1100

    responseCards template

---
+++ responseCards.js
@@ -0,0 +1,165 @@
+const responseCards = [
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+    {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  },
+  {
+    text: "'A common misconception about black cats is that we're bad luck. But that's not actually true!' You speak to them calmly and they apologise for making assumptions about you.",
+    item: 'apple',
+    colour: 'blue'
+  }
+]
+
+
+module.exports = responseCards;
