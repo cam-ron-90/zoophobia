83b0edbcd4a913a3acd81d8aab956ca2c03a9a76
Minh Bui <tranduyminh.bui@gmail.com>
2020-10-26 20:54:37 +1100

    game start with id and players array, need to distribute response cards to each player

--- app.js
+++ app.js
@@ -32,26 +32,26 @@ mongoose.connect(
 // 1. Should cards Object carry graphics for the card or is that keft to front end?
 
 io.on('connect', (socket) => {
-  // socket.on('timer', async({gameID,playerID})=>{
-  //     let countDown = 5;
-  //     let game = await Game.findById(gameID);
-  //     let player = game.players.id(playerID);
-  //     if(player.isPartyLeader){
-  //         let timerID = setInterval(async()=>{
-  //             if(countDown >= 0){
-  //                 io.to(gameID).emit('timer',{countDown,msg : "Starting Game"});
-  //                 countDown--;
-  //             }
-  //             else{
-  //                 game.isOpen = false;
-  //                 game = await game.save();
-  //                 io.to(gameID).emit('updateGame',game);
-  //                 startGameClock(gameID);
-  //                 clearInterval(timerID);
-  //             }
-  //         },1000);
-  //     }
-  // });
+  // Timer and prepare to start game if player is Party leader
+  socket.on('timer', async ({ gameID, playerID }) => {
+    let countDown = 5;
+    let game = await Game.findById(gameID);
+    let player = game.players.id(playerID);
+    if (player.isPartyLeader) {
+      let timerID = setInterval(async () => {
+        if (countDown >= 0) {
+          io.to(gameID).emit('timer', { countDown, msg: 'Starting Game' });
+          countDown--;
+        } else {
+          game.isOpen = false;
+          game = await game.save();
+          io.to(gameID).emit('updateGame', game);
+          startGame(gameID);
+          clearInterval(timerID);
+        }
+      }, 1000);
+    }
+  });
 
   socket.on('deal-cards', async ({ gameID: _id }) => {});
 
@@ -103,38 +103,17 @@ io.on('connect', (socket) => {
   socket.on('send message', (body) => {
     io.emit('message', body);
   });
+
+  //
+  socket.on('deal-cards-to-player', () => {});
 });
 
-// const startGameClock = async (gameID)=>{
-//     let game = await Game.findById(gameID);
-//     game.startTime = new Date().getTime();
-//     game = await game.save();
-//     let time = 120;
-
-//     let timerID = setInterval(function gameIntervalFunc(){
-//         if(time >= 0){
-//             const formatTime = calculateTime(time);
-//             io.to(gameID).emit('timer',{countDown : formatTime,msg : "Time Remaining"});
-//             time--;
-//         }
-//         else{
-//             (async ()=>{
-//                 let endTime = new Date().getTime();
-//                 let game = await Game.findById(gameID);
-//                 let {startTime} = game;
-//                 game.isOver = true;
-//                 game.players.forEach((player,index)=>{
-//                     if(player.WPM === -1)
-//                         game.players[index].WPM = calculateWPM(endTime,startTime,player);
-//                 });
-//                 game = await game.save()
-//                 io.to(gameID).emit('updateGame',game);
-//                 clearInterval(timerID);
-//             })()
-//         }
-//         return gameIntervalFunc;
-//     }(),1000);
-// }
+const startGame = async (gameID) => {
+  let game = await Game.findById(gameID);
+  game.startTime = new Date().getTime();
+  game = await game.save();
+  console.log('game started:', game);
+};
 
 const shuffleArray = (arr) =>
   arr
