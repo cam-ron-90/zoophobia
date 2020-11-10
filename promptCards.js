cbde262d9058de43858bc9da9da7272758f8c837
Cameron Melville <cam.t.melville@gmail.com>
2020-11-10 22:43:12 +1100

    updated promtCards image type

--- promptCards.js
+++ promptCards.js
@@ -2,73 +2,73 @@ const promptCards = [
   {
     text: "Yuck, you black cats should stay at home! You’ll give all the animals bad luck!",
     item: 'apple',
-    frontImg: 'prompt1.png',
+    frontImg: 'prompt1',
     backImg: ''
   },
   {
     text: "Do you kookaburras ever stop laughing? I wish MY life was one big joke...",
     item: 'banana',
-    frontImg: 'prompt2.png',
+    frontImg: 'prompt2',
     backImg: ''
   },
   {
     text: "An echidna sees a Tasmanian devil wearing echidna quill earrings and is upset! Echidnas need quills to protect themselves...",
     item: 'grapes',
-    frontImg: 'prompt3.png',
+    frontImg: 'prompt3',
     backImg: ''
   },
   {
     text: "You! Clean up the catnip spill in Aisle 4! Here’s a reminder, we both know goldfish have terrible memories.",
     item: 'cauliflower',
-    frontImg: 'prompt4.png',
+    frontImg: 'prompt4',
     backImg: ''
   },
   {
     text: "A chick shopping with their siblings and Mama Hen sees a fox with a ‘Don’t Be A Chicken’ t-shirt.",
     item: 'eggplant',
-    frontImg: 'prompt5.png',
+    frontImg: 'prompt5',
     backImg: ''
   },
   {
     text: "Check this out! Is it a cat? Is it a fish? What even are you dude?! Teenage tom cats see a catfish and race up the aisle. They bully her, filming it on Instapaw Live for their followers to watch.",
     item: 'blueberries',
-    frontImg: 'prompt6.png',
+    frontImg: 'prompt6',
     backImg: ''
   },
   {
     text: "The echidna security guard is following a family of ants around the Mega Mart, looking at them suspiciously. The ants are confused - they aren’t doing anything wrong.",
     item: 'carrot',
-    frontImg: 'prompt7.png',
+    frontImg: 'prompt7',
     backImg: ''
   },
   {
     text: "Oi get out of the way! Why don’t you go back to the Pacific Ocean while you’re at it! A chihuahua in a hurry pushes their trolley towards an octopus, who is scared and becomes camouflaged. The octopus gets tangled up in the trolley.",
     item: 'milk',
-    frontImg: 'prompt8.png',
+    frontImg: 'prompt8',
     backImg: ''
   },
   {
     text: "Will a kiss turn you into the horse of my dreams? A horse security guard whose favourite fairytale is ‘The Frog Prince’ tries to kiss a cane toad entering the Mega Mart.",
     item: 'water',
-    frontImg: 'prompt9.png',
+    frontImg: 'prompt9',
     backImg: ''
   },
   {
     text: "All elephants have to enter the Mega Mart from the warehouse loading dock at the back of the store. This is a danger hazard for them, and also makes them feel bad for being treated differently.",
     item: 'toilet paper',
-    frontImg: 'prompt10.png',
+    frontImg: 'prompt10',
     backImg: ''
   },
   {
     text: "There is a native animals’ Acknowledgement sign outsidethe Mega Mart welcoming all animals. Underneath, the Mega Mart manager has put up signs banning animals he doesn’t like from entering the store",
     item: 'bread',
-    frontImg: 'prompt11.png',
+    frontImg: 'prompt11',
     backImg: ''
   },
   {
     text: "“No way, your honey is a bestseller at the Mega Mart! Get out of here...Run back to your Queen! A bee community get inspired by the chickens and echidnas reclaiming theirs eggs from the store. The bees ask the Mega Mart store owner if they can take their honey off the shelves and sell it themselves.",
     item: 'cheese',
-    frontImg: 'prompt12.png',
+    frontImg: 'prompt12',
     backImg: ''
   }
 ]
